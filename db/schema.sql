615909158143a37a1253f28260324c9c7a2589c0
Maryam Muska <maryammuska@ucla.edu>
2022-11-17 13:40:54 -0800

    fixed errors in schema

--- db/schema.sql
+++ db/schema.sql
@@ -7,20 +7,23 @@ CREATE TABLE department(
     department_name VARCHAR(50) NOT NULL
 );
 
-CREATE TABLE roles (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(50) NOT NULL,
+CREATE TABLE role(
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(50),
     salary DECIMAL NOT NULL,
-    department_id INT NOT NULL,
+    department_id INT,
+    PRIMARY KEY(id),
     FOREIGN KEY(department_id) REFERENCES department(id)
+    
 );
 
 CREATE TABLE employee(
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY
+    id INT NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
-    role_id INT NOT NULL,
+    role_id INT,
     manager_id INT,
-    FOREIGN KEY(role_id) REFERENCES role(id),
+    PRIMARY KEY(id),
+    FOREIGN KEY (role_id) REFERENCES role(id),
     FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
