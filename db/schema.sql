e1e3471bb48f7c402c8b1ed21ab11a55b27b5c5b
Maryam Muska <maryammuska@ucla.edu>
2022-11-17 12:14:59 -0800

    changes to seed and schema

--- db/schema.sql
+++ db/schema.sql
@@ -3,22 +3,27 @@ CREATE DATABASE employeetracker_db;
 
 USE employeetracker_db;
 CREATE TABLE department(
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT,
     department_name VARCHAR(50) NOT NULL
+    PRIMARY KEY(id)
 );
 
 CREATE TABLE roles (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(50) NOT NULL,
     salary DECIMAL NOT NULL,
-    department_id INT NOT NULL
-    --FOREIGN KEY
+    department_id INT NOT NULL,
+    PRIMARY KEY(id),
+    FOREIGN KEY(department_id) REFERENCES department(id)
 );
 
 CREATE TABLE employee(
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
     role_id INT NOT NULL,
-    manager_id INT
+    manager_id INT,
+    PRIMARY KEY(id),
+    FOREIGN KEY(role_id) REFERENCES role(id),
+    FOREING KEY (manager_id) REFERENCES employee(id)
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,21 @@
 USE employeetracker_db;
 
 INSERT INTO department(department_name)
+VALUES ("Engineering"),
+       ("Finance"),
+       ("Sales"),
+       ("Legal"); 
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Software Engineer", 150000, 1),
+       ("Accountant", 100000, 2),
+       ("Sales Lead", 90000, 3),
+       ("Lead Lawyer", 120000, 4);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Maryam", "Muska", 1, 1),
+       ("Asmael", "Haidar", 2, 2),
+       ("John", "Haidar", 3, 3),
+       ("Abrahem", "Haidar", 4, 4);
+
+-- can set null for the manager id if not manager
